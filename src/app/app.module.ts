e99723b55d5e38396465adbe1c2a67633329e368
kferrante187 <kferrante187@gmail.com>
2022-10-25 14:31:15 -0400

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MastheadComponent } from './components/masthead/masthead.component';
+import { DumbCounterComponent } from './components/dumb-counter/dumb-counter.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { UiModule } from './features/ui/ui.module';
+import { AuthModule } from './features/auth/auth.module';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+@NgModule({
+  declarations: [
+    // components
+    AppComponent,
+    MastheadComponent,
+    DumbCounterComponent,
+    DashboardComponent,
+    NavigationComponent,
+  ],
+  imports: [
+    // use these other modules
+    BrowserModule,
+    AppRoutingModule,
+    UiModule,
+    AuthModule,
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument(),
+  ],
+  providers: [],
+  bootstrap: [AppComponent], // root level module has this.
+})
+export class AppModule {}
